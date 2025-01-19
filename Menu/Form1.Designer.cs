328f400269ac263ada6e8f361ef79fce666c97c0
Francisco Ramirez <franciscort0424@gmail.com>
2025-01-19 11:08:42 -0400

    ajustes menores

--- Menu/Form1.Designer.cs
+++ Menu/Form1.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(127, 38);
+            this.panel1.Location = new System.Drawing.Point(169, 47);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(645, 400);
+            this.panel1.Size = new System.Drawing.Size(860, 492);
             this.panel1.TabIndex = 3;
             // 
             // button3
@@ -46,9 +47,10 @@
             this.button3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Image = global::Menu.Properties.Resources.menu_regular_24;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button3.Location = new System.Drawing.Point(32, 272);
+            this.button3.Location = new System.Drawing.Point(43, 335);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(70, 55);
+            this.button3.Size = new System.Drawing.Size(93, 68);
             this.button3.TabIndex = 5;
             this.button3.Text = "Historial";
             this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -59,9 +61,10 @@
             this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Image = global::Menu.Properties.Resources.credit_card_regular_24;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button2.Location = new System.Drawing.Point(32, 188);
+            this.button2.Location = new System.Drawing.Point(43, 231);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 55);
+            this.button2.Size = new System.Drawing.Size(93, 68);
             this.button2.TabIndex = 4;
             this.button2.Text = "Pagar";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -72,9 +75,10 @@
             this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Image = global::Menu.Properties.Resources.dollar_regular_24;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button1.Location = new System.Drawing.Point(32, 106);
+            this.button1.Location = new System.Drawing.Point(43, 130);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(70, 55);
+            this.button1.Size = new System.Drawing.Size(93, 68);
             this.button1.TabIndex = 0;
             this.button1.Text = "Nuevo";
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -82,13 +86,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
