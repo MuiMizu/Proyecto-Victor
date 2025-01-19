600cac84f2cffc3b0225390d622bdb9a1d7caf5a
Fabian <hermanoslorenzop2@gmail.com>
2025-01-19 11:31:28 -0400

    Crear Registro RenombrarLogin

--- Login/FormLogin.Designer.cs
+++ Login/FormLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Login
 {
-    partial class Form1
+    partial class FormLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
