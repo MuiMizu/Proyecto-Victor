152e1bdde16b94dde4dac34fdf444e7a73f1e64c
Francisco Ramirez <franciscort0424@gmail.com>
2025-01-20 19:35:50 -0400

    Botones del menu funcionando

--- Menu/Form1.Designer.cs
+++ Menu/Form1.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelContenedor = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelContenedor
             // 
-            this.panel1.Location = new System.Drawing.Point(169, 47);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(860, 492);
-            this.panel1.TabIndex = 3;
+            this.panelContenedor.Location = new System.Drawing.Point(169, 47);
+            this.panelContenedor.Margin = new System.Windows.Forms.Padding(4);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(1224, 982);
+            this.panelContenedor.TabIndex = 3;
             // 
             // button3
             // 
@@ -48,13 +48,14 @@
             this.button3.Image = global::Menu.Properties.Resources.menu_regular_24;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.button3.Location = new System.Drawing.Point(43, 335);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(93, 68);
             this.button3.TabIndex = 5;
             this.button3.Text = "Historial";
             this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -62,13 +63,14 @@
             this.button2.Image = global::Menu.Properties.Resources.credit_card_regular_24;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.button2.Location = new System.Drawing.Point(43, 231);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(93, 68);
             this.button2.TabIndex = 4;
             this.button2.Text = "Pagar";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -76,24 +78,25 @@
             this.button1.Image = global::Menu.Properties.Resources.dollar_regular_24;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.button1.Location = new System.Drawing.Point(43, 130);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(93, 68);
             this.button1.TabIndex = 0;
             this.button1.Text = "Nuevo";
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1406, 1055);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelContenedor);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -103,7 +106,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelContenedor;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
     }
--- Menu/Form1.cs
+++ Menu/Form1.cs
@@ -16,5 +16,33 @@ namespace Menu
         {
             InitializeComponent();
         }
+
+        private void AbrirForm (object formAlt)
+        {
+            if (this.panelContenedor.Controls.Count > 0)
+                this.panelContenedor.Controls.RemoveAt(0);
+            Form fa = formAlt as Form;
+            fa.TopLevel = false;
+            fa.Dock = DockStyle.Fill;
+            this.panelContenedor.Controls.Add(fa);
+            this.panelContenedor.Tag = fa;
+            fa.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AbrirForm(new Factura.Form1());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AbrirForm(new Formulario_Pago.Form1());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AbrirForm(new Amortizacion.Form1());
+        }
     }
 }
